77fe5f978a1522be517af068d8e2471d424d73f8
Kenny Lau <kc_kennylau@yahoo.com.hk>
2018-02-03 00:32:30 +0000

    Update localization.lean

--- localization.lean
+++ localization.lean
@@ -1,6 +1,9 @@
-import data.set.basic tactic.ring
+import data.set.basic tactic.ring data.equiv
 
-universe u
+-- remove "data.equiv" in PR version
+-- needs hom and prime ideals
+
+universes u v
 
 namespace loc
 
@@ -88,4 +91,180 @@ by refine
   existsi is_submonoid.one_mem S,
   ring }
 
+instance : has_coe α (loc α S) := ⟨λ r, ⟦⟨r, 1, is_submonoid.one_mem S⟩⟧⟩
+
+local infix ^ := monoid.pow
+
+variable {α}
+
+def powers (x : α) : set α := {y | ∃ n, x^n = y}
+
+instance powers.is_submonoid (x : α) : is_submonoid α (powers x) :=
+{ one_mem := ⟨0, by simp⟩,
+  mul_mem := λ x₁ x₂ ⟨n₁, hn₁⟩ ⟨n₂, hn₂⟩, ⟨n₁ + n₂, by simp [pow_add, *]⟩ }
+
+def away (x : α) := loc α (powers x)
+
+def closure (S : set α) : set α := {y | ∃ (L:list α) (H:∀ x ∈ L, x ∈ S), L.prod = y }
+
+instance closure.is_submonoid (S : set α) : is_submonoid α (closure S) :=
+{ one_mem := ⟨[], by simp⟩,
+  mul_mem := λ x₁ x₂ ⟨L₁, hLS₁, hL₁⟩ ⟨L₂, hLS₂, hL₂⟩,
+    ⟨L₁ ++ L₂,
+     λ x hx, or.cases_on (list.mem_append.1 hx) (hLS₁ x) (hLS₂ x),
+     by simp [list.prod_append, *]⟩ }
+
+variable α
+
+def non_zero_divisors : set α := {x | ∀ z, x * z = 0 → z = 0}
+
+instance non_zero_divisors.is_submonoid : is_submonoid α (non_zero_divisors α) :=
+{ one_mem := λ z hz, by simpa using hz,
+  mul_mem := λ x₁ x₂ hx₁ hx₂ z hz,
+    have x₁ * (x₂ * z) = 0,
+    by rwa mul_assoc at hz,
+    hx₂ z $ hx₁ (x₂ * z) this }
+
+def quotient_ring := loc α (non_zero_divisors α)
+
+section quotient_ring
+
+variables {β : Type u} [integral_domain β] [decidable_eq β]
+
+lemma ne_zero_of_mem_non_zero_divisors {x : β} :
+  x ∈ loc.non_zero_divisors β → x ≠ 0 :=
+λ hm hz, have x * 1 = 0, by simp [hz], zero_ne_one (hm 1 this).symm
+
+lemma eq_zero_of_ne_zero_of_mul_eq_zero {x y : β} : x ≠ 0 → x * y = 0 → y = 0 :=
+λ hnx hxy, match eq_zero_or_eq_zero_of_mul_eq_zero hxy with
+| or.inl hx := false.elim $ hnx hx
+| or.inr hy := hy
+end
+
+lemma mem_non_zero_divisors_of_ne_zero {x : β} :
+  x ≠ 0 → x ∈ loc.non_zero_divisors β :=
+λ hnx z, eq_zero_of_ne_zero_of_mul_eq_zero hnx
+
+variable β
+
+def inv_aux : β × (non_zero_divisors β) → quotient_ring β :=
+λ ⟨r, s, hs⟩, if h : r = 0 then
+  ⟦⟨0, 1, is_submonoid.one_mem _⟩⟧
+else ⟦⟨s, r, mem_non_zero_divisors_of_ne_zero h⟩⟧
+
+def inv : quotient_ring β → quotient_ring β :=
+quotient.lift (inv_aux β) $
+λ ⟨r₁, s₁, hs₁⟩ ⟨r₂, s₂, hs₂⟩ ⟨t, hts, ht⟩,
+begin
+  have hrs : r₁ * s₂ - r₂ * s₁ = 0,
+  { from hts _ ht },
+  by_cases hr₁ : r₁ = 0;
+  by_cases hr₂ : r₂ = 0;
+  simp [inv_aux, hr₁, hr₂],
+  { exfalso,
+    simp [hr₁] at hrs,
+    exact ne_zero_of_mem_non_zero_divisors hs₁
+      (eq_zero_of_ne_zero_of_mul_eq_zero hr₂ hrs) },
+  { exfalso,
+    simp [hr₂] at hrs,
+    exact ne_zero_of_mem_non_zero_divisors hs₂
+      (eq_zero_of_ne_zero_of_mul_eq_zero hr₁ hrs) },
+  { exact ⟨1, is_submonoid.one_mem _,
+    by simpa [mul_comm] using congr_arg (λ x, -x) hrs⟩ }
+end
+
+@[simp] lemma quotient.lift_beta {α : Sort u} {β : Sort v} [s : setoid α] (f : α → β) (h : ∀ (a b : α), a ≈ b → f a = f b) (x : α):
+quotient.lift f h (quotient.mk x) = f x := rfl
+
+@[simp] lemma quotient.lift_on_beta {α : Sort u} {β : Sort v} [s : setoid α] (f : α → β) (h : ∀ (a b : α), a ≈ b → f a = f b) (x : α):
+quotient.lift_on (quotient.mk x) f h = f x := rfl
+
+instance quotient_ring.field.of_integral_domain : field (quotient_ring β) :=
+by refine
+{ loc.comm_ring β _ with
+  inv := inv β,
+  zero_ne_one := λ hzo, let ⟨t, hts, ht⟩ := quotient.exact hzo in
+    zero_ne_one (by simpa using hts _ ht : 0 = 1),
+  mul_inv_cancel := quotient.ind _,
+  inv_mul_cancel := quotient.ind _ };
+{ intros x hnx,
+  cases x with x hx,
+  cases hx with z hz,
+  have : x ≠ 0,
+    intro hx,
+    apply hnx,
+    apply quotient.sound,
+    existsi (1:β),
+    existsi is_submonoid.one_mem _,
+    simp [hx],
+    exact non_zero_divisors.is_submonoid β,
+  simp [inv, inv_aux, inv_aux._match_1],
+  rw dif_neg this,
+  apply quotient.sound,
+  existsi (1:β),
+  existsi is_submonoid.one_mem _,
+  ring,
+  exact non_zero_divisors.is_submonoid β }
+
+end quotient_ring
+
 end loc
+
+/- TODO:
+1. Define localization at a prime ideal.
+2. Prove that loc.has_coe is a homomorphism of rings.
+3. Prove that loc is a local ring if localizing at a prime ideal.
+-/
+
+-- Factoids (not to go to mathlib):
+#check rat
+
+def frac_int_to_rat : loc.quotient_ring ℤ → ℚ :=
+λ f, quotient.lift_on f (λ ⟨r, s, hs⟩, rat.mk r s) $
+λ ⟨r₁, s₁, hs₁⟩ ⟨r₂, s₂, hs₂⟩ ⟨t, hts, ht⟩,
+begin
+  have hsnz₁ : s₁ ≠ 0,
+  { from loc.ne_zero_of_mem_non_zero_divisors hs₁ },
+  have hsnz₂ : s₂ ≠ 0,
+  { from loc.ne_zero_of_mem_non_zero_divisors hs₂ },
+  have htnz : t ≠ 0,
+  { from loc.ne_zero_of_mem_non_zero_divisors hts },
+  cases eq_zero_or_eq_zero_of_mul_eq_zero ht with htz hrs,
+  {  exfalso,
+     exact htnz htz },
+  { change rat.mk r₁ s₁ = rat.mk r₂ s₂,
+    rw sub_eq_zero at hrs,
+    rwa rat.mk_eq hsnz₁ hsnz₂ }
+end
+
+lemma coe_denom_ne_zero (r : ℚ) : (↑r.denom:ℤ) ≠ 0 :=
+λ hn, ne_of_gt r.pos $ int.of_nat_inj hn
+
+def frac_int_of_rat : ℚ → loc.quotient_ring ℤ :=
+λ r, ⟦⟨r.num, r.denom, λ z hz,
+or.cases_on (eq_zero_or_eq_zero_of_mul_eq_zero hz)
+  (λ hz, false.elim $ coe_denom_ne_zero r hz)
+  id⟩⟧
+
+theorem frac_int_to_rat_to_frac_int : ∀ f, frac_int_of_rat (frac_int_to_rat f) = f :=
+λ f, quotient.induction_on f $ λ ⟨r, s, hs⟩, quotient.sound
+⟨1, loc.is_submonoid.one_mem _,
+   suffices (rat.mk r s).num * s = r * ↑(rat.mk r s).denom,
+   from show 1 * ((rat.mk r s).num * s - r * ↑(rat.mk r s).denom) = 0,
+     by simp [this],
+   have hnd : (↑(rat.mk r s).denom:ℤ) ≠ 0,
+     from coe_denom_ne_zero $ rat.mk r s,
+   have hns : s ≠ 0,
+     from loc.ne_zero_of_mem_non_zero_divisors hs,
+   have _, from eq.symm $ rat.num_denom $ rat.mk r s,
+   by rwa ← rat.mk_eq hnd hns ⟩
+
+theorem rat_to_frac_int_to_rat : ∀ r, frac_int_to_rat (frac_int_of_rat r) = r :=
+λ ⟨n, d, h, c⟩, eq.symm $ rat.num_denom _
+
+def canonical : equiv (loc.quotient_ring ℤ) (ℚ) :=
+⟨frac_int_to_rat, frac_int_of_rat,
+   frac_int_to_rat_to_frac_int,
+   rat_to_frac_int_to_rat⟩
+
+#print horner
