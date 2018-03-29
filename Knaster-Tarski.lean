8849febf855e83c8d152485164f8c9bf4fb4afc3
Kenny Lau <kc_kennylau@yahoo.com.hk>
2018-03-29 13:07:22 +0800

    Update Knaster-Tarski.lean

--- Knaster-Tarski.lean
+++ Knaster-Tarski.lean
@@ -25,18 +25,18 @@ def fixed_points : set α := { x | f x = x }
 
 namespace fixed_points
 
-def previous (x : α) (H : f x ≤ x) : α :=
+def previous (x : α) : α :=
 Sup { z | z ≤ x ∧ z ≤ f z }
 
 variable {f}
 
-theorem previous.le {x : α} {H : f x ≤ x} : previous f x H ≤ x :=
+theorem previous.le {x : α} : previous f x ≤ x :=
 Sup_le $ λ z hz, hz.1
 
-theorem previous.le_apply {x : α} {H : f x ≤ x} : previous f x H ≤ f (previous f x H) :=
+theorem previous.le_apply {x : α} : previous f x ≤ f (previous f x) :=
 Sup_le $ λ z hz, le_trans hz.2 (is_ord_hom.apply_le_apply_of_le $ le_Sup hz)
 
-theorem previous.fixed {x : α} {H : f x ≤ x} : f (previous f x H) = previous f x H :=
+theorem previous.fixed {x : α} (H : f x ≤ x) : f (previous f x) = previous f x :=
 le_antisymm
   (le_Sup ⟨le_trans (is_ord_hom.apply_le_apply_of_le previous.le) H,
      is_ord_hom.apply_le_apply_of_le previous.le_apply⟩)
@@ -44,18 +44,18 @@ le_antisymm
 
 variable f
 
-def next (x : α) (H : x ≤ f x) : α :=
+def next (x : α) : α :=
 Inf { z | x ≤ z ∧ f z ≤ z }
 
 variable {f}
 
-theorem next.le {x : α} {H : x ≤ f x} : x ≤ next f x H :=
+theorem next.le {x : α} : x ≤ next f x :=
 le_Inf $ λ z hz, hz.1
 
-theorem next.apply_le {x : α} {H : x ≤ f x} : f (next f x H) ≤ next f x H :=
+theorem next.apply_le {x : α} : f (next f x) ≤ next f x :=
 le_Inf $ λ z hz, le_trans (is_ord_hom.apply_le_apply_of_le $ Inf_le hz) hz.2
 
-theorem next.fixed {x : α} {H : x ≤ f x} : f (next f x H) = next f x H :=
+theorem next.fixed {x : α} (H : x ≤ f x) : f (next f x) = next f x :=
 le_antisymm
   next.apply_le
   (Inf_le ⟨le_trans H (is_ord_hom.apply_le_apply_of_le next.le),
@@ -87,32 +87,32 @@ instance : complete_lattice (fixed_points f) :=
   le_trans     := λ x y z, le_trans,
   le_antisymm  := λ x y hx hy, subtype.eq $ le_antisymm hx hy,
 
-  sup          := λ x y, ⟨next f (x.1 ⊔ y.1) (aux1 f x y), next.fixed⟩,
-  le_sup_left  := λ x y, show x.1 ≤ _, from le_trans le_sup_left (@next.le _ _ f _ _ (aux1 f x y)),
-  le_sup_right := λ x y, show y.1 ≤ _, from le_trans le_sup_right (@next.le _ _ f _ _ (aux1 f x y)),
+  sup          := λ x y, ⟨next f (x.1 ⊔ y.1), next.fixed (aux1 f x y)⟩,
+  le_sup_left  := λ x y, show x.1 ≤ _, from le_trans le_sup_left next.le,
+  le_sup_right := λ x y, show y.1 ≤ _, from le_trans le_sup_right next.le,
   sup_le       := λ x y z hxz hyz, Inf_le ⟨sup_le hxz hyz, z.2.symm ▸ le_refl _⟩,
 
-  inf          := λ x y, ⟨previous f (x.1 ⊓ y.1) (aux2 f x y), previous.fixed⟩,
-  inf_le_left  := λ x y, show _ ≤ x.1, from le_trans (@previous.le _ _ f _ _ (aux2 f x y)) inf_le_left,
-  inf_le_right := λ x y, show _ ≤ y.1, from le_trans (@previous.le _ _ f _ _ (aux2 f x y)) inf_le_right,
+  inf          := λ x y, ⟨previous f (x.1 ⊓ y.1), previous.fixed (aux2 f x y)⟩,
+  inf_le_left  := λ x y, show _ ≤ x.1, from le_trans previous.le inf_le_left,
+  inf_le_right := λ x y, show _ ≤ y.1, from le_trans previous.le inf_le_right,
   le_inf       := λ x y z hxy hxz, le_Sup ⟨le_inf hxy hxz, x.2.symm ▸ le_refl _⟩,
 
-  top          := ⟨previous f ⊤ le_top, previous.fixed⟩,
+  top          := ⟨previous f ⊤, previous.fixed le_top⟩,
   le_top       := λ ⟨x, H⟩, le_Sup ⟨le_top, H.symm ▸ le_refl x⟩,
 
-  bot          := ⟨next f ⊥ bot_le, next.fixed⟩,
+  bot          := ⟨next f ⊥, next.fixed bot_le⟩,
   bot_le       := λ ⟨x, H⟩, Inf_le ⟨bot_le, H.symm ▸ le_refl x⟩,
 
-  Sup          := λ A, ⟨next f (Sup $ subtype.val '' A) (aux3 f A), next.fixed⟩,
+  Sup          := λ A, ⟨next f (Sup $ subtype.val '' A), next.fixed (aux3 f A)⟩,
   le_Sup       := λ A x hxA, show x.1 ≤ _, from le_trans
                     (le_Sup $ show _ ∈ subtype.val '' A, from ⟨x, hxA, rfl⟩)
-                    (@next.le _ _ f _ _ (aux3 f A)),
+                    next.le,
   Sup_le       := λ A x Hx, Inf_le ⟨Sup_le $ λ z ⟨y, hyA, hyz⟩, hyz ▸ Hx _ hyA, x.2.symm ▸ le_refl _⟩,
 
-  Inf          := λ A, ⟨previous f (Inf $ subtype.val '' A) (aux4 f A), previous.fixed⟩,
+  Inf          := λ A, ⟨previous f (Inf $ subtype.val '' A), previous.fixed (aux4 f A)⟩,
   le_Inf       := λ A x Hx, le_Sup ⟨le_Inf $ λ z ⟨y, hyA, hyz⟩, hyz ▸ Hx _ hyA, x.2.symm ▸ le_refl _⟩,
   Inf_le       := λ A x hxA, show _ ≤ x.1, from le_trans
-                    (@previous.le _ _ f _ _ (aux4 f A))
+                    previous.le
                     (Inf_le $ show _ ∈ subtype.val '' A, from ⟨x, hxA, rfl⟩) }
 
 end fixed_points
